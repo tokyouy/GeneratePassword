b4716e33b49be311f0f00b804ecb1fe2f9b85825
tokyouy <tokyouy@DESKTOP-4FLTK79>
2021-12-14 10:51:36 +0900

    Add project files.

---
+++ GeneratePassword/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeneratePassword
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string genpass()
+        {
+            Random random = new Random();
+            string[] pass_string = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "!", "@", "#", "$", "%", "^", "&", "*"};
+            string gennerated = "";
+            for (int i = 0; i < 16; i++)
+            {
+                gennerated += pass_string[random.Next(pass_string.Length)];
+            }
+            return gennerated;
+        }
+
+        private void btnGenPassword_Click(object sender, RoutedEventArgs e)
+        {
+            tbPass.Text = genpass();
+        }
+    }
+}
